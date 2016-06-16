06d1d613bf5295dea8ccccc9be9fee20a7b95875
Adland Lee <adland@4mfd.com>
2016-06-16 16:26:01 -0700

    create central panel

--- SlideOutNavigation/ContainerViewController.swift
+++ SlideOutNavigation/ContainerViewController.swift
@@ -10,26 +10,64 @@ import UIKit
 import QuartzCore
 
 class ContainerViewController: UIViewController {
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-  
+    
+    var centerNavigationController: UINavigationController!
+    var centerViewController: CenterViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        centerViewController = UIStoryboard.centerViewController()
+        centerViewController.delegate = self
+        
+        centerNavigationController = UINavigationController(rootViewController: centerViewController)
+        view.addSubview(centerNavigationController.view)
+        addChildViewController(centerNavigationController)
+        
+        centerNavigationController.didMoveToParentViewController(self)
+    }
+    
+}
+
+extension ContainerViewController: CenterViewControllerDelegate {
+    func toggleLeftPanel() {
+        
+    }
+    
+    func toggleRightPanel() {
+        
+    }
+    
+    func addLeftPanelViewController() {
+    
+    }
+    
+    func addRightPanelViewController() {
+        
+    }
+    
+    func animateLeftPanel(shouldExpand: Bool) {
+        
+    }
+    
+    func animateRightPanel(shouldExpand: Bool) {
+        
+    }
 }
 
 private extension UIStoryboard {
-  class func mainStoryboard() -> UIStoryboard { return UIStoryboard(name: "Main", bundle: NSBundle.mainBundle()) }
-  
-  class func leftViewController() -> SidePanelViewController? {
-    return mainStoryboard().instantiateViewControllerWithIdentifier("LeftViewController") as? SidePanelViewController
-  }
-  
-  class func rightViewController() -> SidePanelViewController? {
-    return mainStoryboard().instantiateViewControllerWithIdentifier("RightViewController") as? SidePanelViewController
-  }
-  
-  class func centerViewController() -> CenterViewController? {
-    return mainStoryboard().instantiateViewControllerWithIdentifier("CenterViewController") as? CenterViewController
-  }
-  
+    class func mainStoryboard() -> UIStoryboard { return UIStoryboard(name: "Main", bundle: NSBundle.mainBundle()) }
+    
+    class func leftViewController() -> SidePanelViewController? {
+        return mainStoryboard().instantiateViewControllerWithIdentifier("LeftViewController") as? SidePanelViewController
+    }
+    
+    class func rightViewController() -> SidePanelViewController? {
+        return mainStoryboard().instantiateViewControllerWithIdentifier("RightViewController") as? SidePanelViewController
+    }
+    
+    class func centerViewController() -> CenterViewController? {
+        return mainStoryboard().instantiateViewControllerWithIdentifier("CenterViewController") as? CenterViewController
+    }
+    
 }
